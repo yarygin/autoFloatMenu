f2f19de94efd60d528b5ddf9130db51d1400b1ea
Alexander Yarygin <yarygin@raspberrypi.(none)>
2013-12-17 15:06:30 +0400

    added flowable and autoGenerate parameters

--- JQuery.autoFloatMenu.js
+++ JQuery.autoFloatMenu.js
@@ -1,27 +1,38 @@
 (function( $ ){
 	$.fn.autoFloatMenu = function(options) {
 		var that = this;
-		$(document).scroll(function() {
-			$(that).stop();
-			$(that).animate({
-				top: ($(this).scrollTop())
-			});
-		});
 		var defaults = {
 			blockSelector: ".content",
 			titleSelector: "h3",
-			menuElClass: "menu-item"
+			menuElClass: "menu-item",
+			flowable:true,
+			autoGenerate:true
 		}
 		settings = $.extend({}, defaults, options);
-		$(settings.blockSelector).each(function(key, item){
-			that.append(
-				$("<nav/>").append(
-					$("<a/>")
-						.text($(item).find(settings.titleSelector).text())
-						.attr("href","#"+$(item).attr("id"))
-						.addClass(settings.menuElClass)
-				)
-			);
-		});
+		
+		if(settings.flowable)
+		{
+			that.css("position", that.css("position")=="absolute"?"absolute":"relative");
+
+			$(document).scroll(function() {
+				that.stop();
+				that.animate({
+					top: ($(this).scrollTop())
+				});
+			});
+		}
+		
+		if(settings.autoGenerate)
+		{
+			that.append($("<nav/>"));
+			$(settings.blockSelector).each(function(key, item){
+				that.find("nav").first().append(
+						$("<a/>")
+							.text($(item).find(settings.titleSelector).first().text())
+							.attr("href","#"+$(item).attr("id"))
+							.addClass(settings.menuElClass)
+					)
+			});
+		}
 	};
-})(jQuery);
+})( jQuery );
